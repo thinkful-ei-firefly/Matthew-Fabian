f2c5f03ce363eb5056ad5e689775b2781b371b44
Fabian Lema <fabianlemac@gmail.com>
2019-07-10 10:45:04 -0500

    Added KeyDeleter function

--- objects_drill.js
+++ objects_drill.js
@@ -27,3 +27,21 @@ const personMaker = () => ({
 });
 
 const person1 = personMaker();
+
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  // your code here
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+console.log(keyDeleter(sampleObj));
+
