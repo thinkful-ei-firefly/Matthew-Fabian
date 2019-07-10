8d6c5a7c3886d1dc29e6fcf7051ca5ecb5a1e761
Fabian Lema <fabianlemac@gmail.com>
2019-07-10 10:25:13 -0500

    Addesd ModifyObject function

--- objects_drill.js
+++ objects_drill.js
@@ -7,3 +7,13 @@ const createMyObject = () => ({
 
 const obj = createMyObject();
 console.log(obj);
+
+/*const modifyObject() = (obj) => ({
+	return 
+});*/
+
+function modifyObject(obj){
+	return {...obj, foo:'foo', bar:'bar', bizz:'bizz', bang:'bang'};
+}
+
+console.log(modifyObject(obj));
