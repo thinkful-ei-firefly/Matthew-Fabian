2e1fa204f3bff5b2994eab47edf10d2e77a63b87
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-10 11:16:23 -0400

    object creator

---
+++ objects_drill.js
@@ -0,0 +1,9 @@
+const createMyObject = () => ({
+  foo: "bar",
+  answerToUniverse: 42,
+  "olly olly": "oxen free",
+  sayHello: () => "hello"
+});
+
+const obj = createMyObject();
+console.log(obj);
